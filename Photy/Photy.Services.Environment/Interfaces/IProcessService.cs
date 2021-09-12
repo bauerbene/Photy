a5b1d81d46082155fdf36b180d09fe8f00667f6b
Benedikt Bauer <benedikt.bauer@posteo.de>
2021-09-12 13:06:15 +0200

    added interface documentation to IProcessService

--- Photy/Photy.Services.Environment/Interfaces/IProcessService.cs
+++ Photy/Photy.Services.Environment/Interfaces/IProcessService.cs
@@ -4,8 +4,34 @@ namespace Photy.Services.Environment.Interfaces
 {
     public interface IProcessService
     {
+        /// <summary>
+        ///     Execute a command with specified arguments.
+        /// </summary>
+        /// 
+        /// <param name="command">
+        ///     The command which should be executed.
+        /// </param>
+        /// 
+        /// <param name="arguments">
+        ///     The arguments for the command.
+        /// </param>
         void Execute(string command, string arguments);
 
+        /// <summary>
+        ///     Executes a command async and returns the output of the corresponding process.
+        /// </summary>
+        /// 
+        /// <param name="command">
+        ///     The command which should be executed.
+        /// </param>
+        /// 
+        /// <param name="arguments">
+        ///     The arguments for the command.
+        /// </param>
+        /// 
+        /// <returns>
+        ///     The output of the command.
+        /// </returns>
         Task<string> ExecuteAndGetOutput(string command, string arguments);
     }
 }
